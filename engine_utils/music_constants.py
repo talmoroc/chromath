d7833c995dbb2893857889c95aa0e619f3de5d39
talmoroc <61518583+talmoroc@users.noreply.github.com>
2026-01-23 20:18:43 +0100

    Add constants

--- engine_utils/music_constants.py
+++ engine_utils/music_constants.py
@@ -7,6 +7,8 @@ if TYPE_CHECKING:
 
 A4_MIDI_VALUE = 69
 A4_FREQ = 440.0
+TUNING = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)  # Chromatic scale semitones
+IONIAN_SEMITONES = [0, 2, 4, 5, 7, 9, 11]
 
 def pitch_distance(p1: int | Pitch, p2: int | Pitch) -> int:
     """ Distance between two notes (defined by semitones)
