46fa1612adf36b3e7ce2cc440a7f8d0aed63474e
talmoroc <61518583+talmoroc@users.noreply.github.com>
2026-01-27 18:54:17 +0100

    update music system

--- engine_utils/music_system.py
+++ engine_utils/music_system.py
@@ -1,5 +1,7 @@
+from typing import Any
+from numpy.typing import NDArray, DTypeLike
 from dataclasses import dataclass, field
-from contextvars import ContextVar
+from contextvars import ContextVar, Token
 from types import TracebackType
 import numpy as np
 
@@ -8,25 +10,27 @@ import numpy as np
 class MusicSystem:
     tones: int = 12
     degrees: int = 7
-    max_st: int = 2**7 - 1
-    powers: np.ndarray = field(init=False, repr=False)  # bitwise representation of tones
+    func_degrees: int = 13  # degrees meaningful for intervals
+    powers: NDArray[np.int_] = field(init=False, repr=False)  # bitwise representation of tones
+    max_int_repr: int = field(init=False, repr=False)
 
     def __post_init__(self):
-        powers = 1 << np.arange(self.tones, dtype=object)
+        powers: NDArray = 1 << np.arange(self.tones, dtype=object)
         object.__setattr__(self, 'powers', powers)
-        object.__setattr__(self, 'max_st', powers[-1])
+        object.__setattr__(self, 'max_int_repr', int(powers.sum()))
 
 
-_current_system = ContextVar('music_system', default=MusicSystem(12, 7))
+_current_system = ContextVar('music_system', default=MusicSystem())
 
 
+# DYNAMIC LOADING OF A MUSIC SYSTEM
 class use_system:
     def __init__(self, tones: int = 12, degrees: int = 7):
-        self.token = None
+        self.token: Token[MusicSystem] | None = None
         self.new_system = MusicSystem(tones, degrees)
 
     def __enter__(self) -> MusicSystem:
-        self.token = _current_system.set(self.new_system)
+        self.token: Token[MusicSystem] = _current_system.set(self.new_system)
         return self.new_system
 
     def __exit__(self, exc_type: type[BaseException] | None, exc_val: BaseException | None, exc_tb: TracebackType | None):
@@ -36,3 +40,6 @@ class use_system:
 
 def get_current_music_system() -> MusicSystem:
     return _current_system.get()
+
+
+__all__ = ['get_current_music_system', 'use_system']
