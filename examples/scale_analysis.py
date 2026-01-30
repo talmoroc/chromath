bbde41df956ca848025132e9e3afb63ba4cac73b
talmoroc <61518583+talmoroc@users.noreply.github.com>
2026-01-30 19:49:28 +0100

    example to check package later

--- examples/scale_analysis.py
+++ examples/scale_analysis.py
@@ -1,31 +1,17 @@
 import mido
 
-from engine_utils.dissonance_computation import *
-from music_objects import *
 
-
+# WIP
 def find_port_name() -> str:
     for name in mido.get_output_names():  # type: ignore
-        if 'Python MIDI' in name:
+        if "Python MIDI" in name:
             return name
     raise RuntimeError("MIDI output port 'Python MIDI' not found.")
 
 
-
 def __main__():
     port_name = find_port_name()
-    outport = mido.open_output(port_name)  # type: ignore
+    outport = mido.open_output(port_name)  # type: ignore  # noqa: F841
 
-    A4 = Note(69)
-    c = Chord.from_shape(A4, ChordShape.m7)
-    print(c)
-    c2 = c.invert(1)
-    print(c2)
-
-    # test1 = Chord([0, 4, 7])
-    # test = Chord.from_shape(2, ChordShape.MINOR)
-    # test1.play(outport)
-    # test.play(outport)
-    # print(test)
 
 __main__()
