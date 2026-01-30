7ef2a829a3ad19a885952283ceeccb0a69f497d0
talmoroc <61518583+talmoroc@users.noreply.github.com>
2026-01-30 19:47:23 +0100

    Separate midi api concerns (not in the music theory objects)

---
+++ src/chordvoyager/playback/midi_api.py
@@ -0,0 +1,16 @@
+import mido
+import time
+import threading
+
+def _play_unthreaded(self, outport, duration: int = 500, velocity: int = 80):
+    for n in self.midi:
+        outport.send(mido.Message("note_on", note=n, velocity=velocity))
+    time.sleep(duration / 1000)
+    for n in self.midi:
+        outport.send(mido.Message("note_off", note=n, velocity=velocity))
+
+def play(self, duration: int = 500, velocity: int = 80):
+    thread = threading.Thread(
+        target=self._play_unthreaded, args=(duration, velocity)
+    )
+    thread.start()
