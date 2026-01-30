7d882e7abfd78fd0358c08e1fe43ba22d5a900f3
talmoroc <61518583+talmoroc@users.noreply.github.com>
2026-01-30 19:05:52 +0100

    Renaming music_objects to music_theory_objects

--- src/chordvoyager/components/music_theory_objects.py
+++ src/chordvoyager/components/music_theory_objects.py
@@ -1,24 +1,27 @@
-import itertools
+import threading
 import time
 import mido
-import threading
+from functools import cached_property
 
-from engine_utils.base_objects import *
-from engine_utils.chord_shapes import *
-import engine_utils.dissonance_computation as utils
+from src.chordvoyager.constants import ChordShape, Pitch, Interval
+import src.chordvoyager.core.freq_ops as freq_op
+import src.chordvoyager.core.degree_ops as deg_op
+import src.chordvoyager.core.chroma_ops as chr_op
+
 
 
 class Note:
     def __init__(self, midi_value: int):
         if midi_value < 0 or midi_value > 127:
             raise ValueError(
-                f'MIDI note must be between 0 and 127 - value {midi_value}')
+                f"MIDI note must be between 0 and 127 - value {midi_value}"
+            )
         self.midi = midi_value
         self.note_index = self.midi % 12
         self.octave = self.midi // 12
-        self.pitch = Pitch.from_midi(self.midi)
+        self.pitch = Pitch(self.note_index, 0)
         self.name = str(self.pitch)
-        self.freq = utils.midi_freq(self.midi)
+        self.freq = freq_op.midi_freq(self.midi)
 
     def shift(self, semitones: int = 0, octaves: int = 0) -> Note:
         return Note(self.midi + semitones + 12 * octaves)
@@ -57,29 +60,35 @@ class Note:
     @classmethod
     def from_pitch(cls, pitch: Pitch, octave: int = 0):
         return cls(pitch.sounding_pitch + (octave + 1) * 12)
+    
+    @cached_property
+    def chroma(self) -> Chroma:
+        return Chroma(deg_op.to_chroma(self.note_index))
 
 
 class Chord:
     def __init__(self, notes: list[Note]):
         if len(notes) < 2:
             raise ValueError(
-                f'{len(notes)} notes given. At least 2 notes must be provided to build a Chord.')
+                f"{len(notes)} notes given. At least 2 notes must be provided to build a Chord."
+            )
         self.notes = sorted(notes)
         self.lowest_note = min(self.notes)
         self.octave = self.lowest_note.octave  # Temporary octave assignment
         self.midi = [note.midi for note in self.notes]
-        self.semitones = [(note - self.lowest_note).midi %
-                          12 for note in self.notes]
+        self.semitones = [(note - self.lowest_note).midi % 12 for note in self.notes]
         self.frequencies = [note.freq for note in self.notes]
         self.intervals = [
-            Interval.P0, Interval.M3, Interval.P5
+            Interval.P0,
+            Interval.M3,
+            Interval.P5,
             # Interval.from_semitones((i - self.lowest_note).note_index) for i in self.notes
         ]
-        self.relative_periodicity = utils.relative_periodicity(self.semitones)
-        self.dissonance = utils.smoothed_relative_periodicity(
-            self.semitones)
-        self._dissonance_raw = utils.smoothed_relative_periodicity(
-            self.semitones, log=False)
+        self.freq_op.relative_periodicity = freq_op.relative_periodicity(self.semitones)
+        self.dissonance = freq_op.smoothed_freq_op.relative_periodicity(self.semitones)
+        self._dissonance_raw = freq_op.smoothed_freq_op.relative_periodicity(
+            self.semitones, log=False
+        )
 
     def __str__(self):
         return f"Root: MIDI Note {self.root.midi} with frequency {self.root.freq} Hz\n {(self.root.pitch)} {self.shape} Chord in inversion {self.inversion}. Ambiguous root: {self.root_is_ambiguous}"
@@ -96,8 +105,14 @@ class Chord:
             inversion = self.invert(index)
             quality = ChordShape.from_intervals(inversion.intervals)
             if quality != ChordShape.UNKNOWN:
-                quality_tuple.append((self.notes[index], quality, (len(
-                    self.notes) - index) % len(self.notes), inversion.dissonance))
+                quality_tuple.append(
+                    (
+                        self.notes[index],
+                        quality,
+                        (len(self.notes) - index) % len(self.notes),
+                        inversion.dissonance,
+                    )
+                )
         return quality_tuple
 
     @property
@@ -153,20 +168,19 @@ class Chord:
         """Compare chords by pitch class (ignoring inversion, octave, and repetition)."""
         if not isinstance(other, Chord):
             return False
-        self_pitches = sorted(
-            {n.pitch for n in self.notes}, key=lambda p: p.value)
-        other_pitches = sorted(
-            {n.pitch for n in other.notes}, key=lambda p: p.value)
+        self_pitches = sorted({n.pitch for n in self.notes}, key=lambda p: p.value)
+        other_pitches = sorted({n.pitch for n in other.notes}, key=lambda p: p.value)
         return self_pitches == other_pitches
 
     def _play_unthreaded(self, outport, duration: int = 500, velocity: int = 80):
         for n in self.midi:
-            outport.send(mido.Message('note_on', note=n, velocity=velocity))
+            outport.send(mido.Message("note_on", note=n, velocity=velocity))
         time.sleep(duration / 1000)
         for n in self.midi:
-            outport.send(mido.Message('note_off', note=n, velocity=velocity))
+            outport.send(mido.Message("note_off", note=n, velocity=velocity))
 
     def play(self, duration: int = 500, velocity: int = 80):
         thread = threading.Thread(
-            target=self._play_unthreaded, args=(duration, velocity))
+            target=self._play_unthreaded, args=(duration, velocity)
+        )
         thread.start()
